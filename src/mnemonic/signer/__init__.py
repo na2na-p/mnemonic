ee42a472d2bad8e323d0a7b478cadeb6b001e2ca
na2na-p <na2na@na2na.dev>
2026-01-31 00:05:44 +0900

    feat(signer): implement DefaultZipalignRunner

--- src/mnemonic/signer/__init__.py
+++ src/mnemonic/signer/__init__.py
@@ -3,9 +3,10 @@
 APK署名に関連する機能を提供します。
 """
 
-from mnemonic.signer.apk import ZipalignError, ZipalignRunner
+from mnemonic.signer.apk import DefaultZipalignRunner, ZipalignError, ZipalignRunner
 
 __all__ = [
+    "DefaultZipalignRunner",
     "ZipalignError",
     "ZipalignRunner",
 ]
